7f0eb6cac1405950c9f5a217addc0080e5b87765
aoifeviolet <aoife11500@googlemail.com>
2023-06-08 21:17:43 +0100

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
-function displayTemperature(response) {console.log(response.data.temperature.current);
+function displayTemperature(response) {console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
+    let descriptionElement = document.querySelector("#tempDescription")
+    let cityElement = document.querySelector("#city")
+    let humidityElement = document.querySelector("#humidity")
+    
     temperatureElement.innerHTML = Math.round(response.data.temperature.current)
+    cityElement.innerHTML = (response.data.city)
+    descriptionElement.innerHTML = (response.data.condition.description)
+    humidityElement.innerHTML = (response.data.temperature.humidity)
 }
 
 let apiKey = "42c46tb11f82fdddoa3422839024da74"
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`
 
 axios.get(apiUrl).then(displayTemperature)
