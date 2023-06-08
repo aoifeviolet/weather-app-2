e8baf49b4203f7ba5b9a233f13fc340102b5d3cb
aoifeviolet <aoife11500@googlemail.com>
2023-06-08 20:56:16 +0100

    Axios
    Integrated Axios and test API call

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-function displayTemperatue(response) {console.log(response.temperature.current);
+function displayTemperature(response) {console.log(response.data.temperature.current);
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(response.daily.current)
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current)
 }
 
 let apiKey = "42c46tb11f82fdddoa3422839024da74"
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`
+
+axios.get(apiUrl).then(displayTemperature)
