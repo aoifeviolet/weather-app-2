31eb6c0cb810016f2f428a62bb621d1015d96f4d
Aoife Murphy <aoife11500@googlemail.com>
2023-07-31 18:23:01 +0100

    Added forecast

--- src/app.js
+++ src/app.js
@@ -9,13 +9,36 @@ function formatDate(timestmap){
     return `${day} ${hours}:${minutes}`
 }
 
-function displayTemperature(response) {console.log(response)
+function displayForecast(forecast) { 
+  
+let forecastElement = document.querySelector("#forecast")
+let forecastHTML = `<div class="row">`;
+let days = ["Sat", "Sun", "Fri"]
+
+days.forEach(function(day) {forecastHTML = forecastHTML + `
+<div class="col-2">
+  <div class="forecast-date">${day}</div>
+  <div class="forecast-temp">
+    30°C
+  </div>
+  <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png" alt="" class="forecast-icon"/>
+</div>`;
+})
+
+forecastHTML = forecastHTML + `</div>`
+
+forecastElement.innerHTML = forecastHTML
+  
+}
+
+function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature")
     let descriptionElement = document.querySelector("#tempDescription")
     let cityElement = document.querySelector("#city")
     let humidityElement = document.querySelector("#humidity")
     let dateElement = document.querySelector("#date")
     let iconElement = document.querySelector("#icon")
+    let windElement = document.querySelector("#wind")
     
     temperatureElement.innerHTML = Math.round(response.data.temperature.current)
     cityElement.innerHTML = (response.data.city)
@@ -23,11 +46,31 @@ function displayTemperature(response) {console.log(response)
     humidityElement.innerHTML = (response.data.temperature.humidity)
     dateElement.innerHTML = formatDate(response.data.time * 1000);
     iconElement.setAttribute("src", `${response.data.condition.icon_url}`)    
+    windElement.innerHTML = (response.data.wind.speed)
 }
 
+function search(city){
 let apiKey = "42c46tb11f82fdddoa3422839024da74"
-let city = "Seoul"
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
-
 axios.get(apiUrl).then(displayTemperature)
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  console.log(cityInputElement.value);
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+
+search("new york")
+displayForecast()
+
+console.log(respone.data)
+
+
+
 
